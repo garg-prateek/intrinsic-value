5242bfb51972b7952c0608872423012e4a751e17
Prateek Garg <garg.prateek123@gmail.com>
2022-08-04 19:42:00 +0530

    List added to store future cash flow

--- src/IntrinsicValue.java
+++ src/IntrinsicValue.java
@@ -11,81 +11,89 @@ public class IntrinsicValue {
 	static double capExCurrent;
 	static double growthRate1;
 	static double growthRate2;
-	static double a;
-	static double b;
+	static double a; // average operating cash for last three years
+	static double b; // average capital expenditure for last three years
 	static double fifthCash;
 	static double tenthCash;
 	static double terminalValue;
 	static double terminalGrowthRate;
 	static double discountRate;
 
-	public static void averageOperatingCashFlow() {
-		System.out.println("Enter the last three years Operating Cash: ");
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter 2 years before Operating Cash");
-		opCashminus2 = s.nextDouble();
-		System.out.println("Enter last year  Operating Cash");
-		opCashminus1 = s.nextDouble();
-		System.out.println("Enter current year  Operating Cash");
-		opCashCurrent = s.nextDouble();
-		a = (opCashminus2 + opCashminus1 + opCashCurrent) / 3;
-		s.close();
-
-	}
-
-	public static void averageCapitalExpenditure() {
-		System.out.println("Enter the last three years Capital Expenditure: ");
-		Scanner s = new Scanner(System.in);
-
-		System.out.println("Enter 2 years before Capital Expenditure: ");
-		capEx2 = s.nextDouble();
-		System.out.println("Enter last year Capital Expenditure: ");
-		capEx1 = s.nextDouble();
-		System.out.println("Enter current year Capital Expenditure: ");
-		capExCurrent = s.nextDouble();
-		b = (capEx2 + capEx1 + capExCurrent) / 3;
-		s.close();
-
-	}
-
-	public static void rate() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the growth rate for first five years");
-		growthRate1 = sc.nextDouble();
-		System.out.println("Enter the growth rate for first last years");
-		growthRate2 = sc.nextDouble();
-		System.out.println("Enter the terminal growth rate for first five years");
-		terminalGrowthRate = sc.nextDouble();
-		System.out.println("Enter the discount  rate for first five years");
-		discountRate = sc.nextDouble();
-
-	}
+	// public static void averageOperatingCashFlow() {
+//		System.out.println("Enter the last three years Operating Cash: ");
+//		Scanner s = new Scanner(System.in);
+//		System.out.println("Enter 2 years before Operating Cash");
+//		opCashminus2 = s.nextDouble();
+//		System.out.println("Enter last year  Operating Cash");
+//		opCashminus1 = s.nextDouble();
+//		System.out.println("Enter current year  Operating Cash");
+//		opCashCurrent = s.nextDouble();
+//		a = (opCashminus2 + opCashminus1 + opCashCurrent) / 3;
+//		s.close();
+//
+//	}
+//
+//	public static void averageCapitalExpenditure() {
+//		System.out.println("Enter the last three years Capital Expenditure: ");
+//		Scanner s = new Scanner(System.in);
+//
+//		System.out.println("Enter 2 years before Capital Expenditure: ");
+//		capEx2 = s.nextDouble();
+//		System.out.println("Enter last year Capital Expenditure: ");
+//		capEx1 = s.nextDouble();
+//		System.out.println("Enter current year Capital Expenditure: ");
+//		capExCurrent = s.nextDouble();
+//		b = (capEx2 + capEx1 + capExCurrent) / 3;
+//		s.close();
+//
+//	}
+//
+//	public static void rate() {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter the growth rate for first five years");
+//		growthRate1 = sc.nextDouble();
+//		System.out.println("Enter the growth rate for first last years");
+//		growthRate2 = sc.nextDouble();
+//		System.out.println("Enter the terminal growth rate for first five years");
+//		terminalGrowthRate = sc.nextDouble();
+//		System.out.println("Enter the discount  rate for first five years");
+//		discountRate = sc.nextDouble();
+//
+//	}
 
 	public static void FCF() {
 		System.out.println("The future cash flow is as follows: ");
-		for (int i = 1; i <= 5; i++) {
-			double sum = a * Math.pow((1 + growthRate1), i);
-			System.out.println("The future cash flow for " + i + "th year is " + sum);
+		ArrayList<Double> list = new ArrayList<>();
 
+		for (int i = 1; i <= 5; i++) {
+			double sum = a * Math.pow((1 + 10), i);
+			System.out.println("The future cash flow for " + i + "th year is " + sum);
+			list.add(sum);
 		}
+
 		fifthCash = a * Math.pow((1 + growthRate1), 5);
 
 		for (int i = 6; i <= 10; i++) {
-			System.out.println(
-					"The future cash flow for " + i + "th year is " + fifthCash * Math.pow((1 + growthRate1), i));
+			double sum1 = fifthCash * Math.pow((1 + growthRate1), i);
+			System.out.println("The future cash flow for " + i + "th year is " + sum1);
+			list.add((i - 1), sum1);
 		}
+		System.out.println(list);
 		tenthCash = a * Math.pow((1 + growthRate1), 10);
 
 		terminalValue = tenthCash * (1 + terminalGrowthRate) / ((discountRate - terminalGrowthRate) / 100);
 
 	}
+	
+	public void netPresentValue() {
+		
+	}
 
 	public static void main(String[] args) {
-		List list = new ArrayList<>();
 
-		averageOperatingCashFlow();
-		averageCapitalExpenditure();
-		rate();
+//		averageOperatingCashFlow();
+//		averageCapitalExpenditure();
+//		rate();
 
 		FCF();
 
